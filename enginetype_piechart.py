a97bf80de6ba410f3b9270b6b62d785d883197f5
Ajay Karmakar <120260061+ajay-karmakar@users.noreply.github.com>
2024-11-14 22:46:46 +0530

    Update enginetype_piechart.py

--- enginetype_piechart.py
+++ enginetype_piechart.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'C:\Users\AJAY KARMAKAR\PycharmProjects\PR391\Automobile_data.csv')
+df = pd.read_csv('Automobile_data.csv')
 print(df.head())
 
 columns_to_plot = ['make', 'engine-location']
